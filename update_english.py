bc44ebf9df584c7343419b41c31bad6360bbb8ee
NicolasGrosjean <grosjen@hotmail.fr>
2019-02-23 09:17:39 +0100

    Improve updating script
    The script work directly with the Steam directory and manage the unzipping

--- update_english.py
+++ update_english.py
@@ -1,19 +1,39 @@
 import os
 import argparse
 import shutil
+import zipfile
+from time import gmtime, strftime
 
 
 def get_args():
     parser = argparse.ArgumentParser(description='Update English files')
     parser.add_argument('steam', type=str, help='Steam mod directory')
     parser.add_argument('mod', type=str, help='Local mod directory')
+    parser.add_argument('-unzip', type=str, help='Unzip directory (default directory upper mod directory)')
     return parser.parse_args()
 
 
-def main(args):
-    # TODO unzip steam archive
+def unzip(path_to_zip_file, directory_to_extract_to):
+    print(f'Beginning of the extraction of {path_to_zip_file}')
+    zip_ref = zipfile.ZipFile(path_to_zip_file, 'r')
+    zip_ref.extractall(directory_to_extract_to)
+    zip_ref.close()
+    print(f'End of the extraction of {path_to_zip_file}')
+    print(f'{directory_to_extract_to} created.')
 
-    unzip_mod_dir = os.path.join('1.2.2')
+
+def main(args):
+    # Compute unzip mod directory
+    unzip_mod_dir = os.path.join(os.path.join(args.mod, '..', strftime("%Y_%m_%d_%H_%M_%S", gmtime())))
+    if args.unzip is not None:
+        unzip_mod_dir = args.unzip
+        if os.path.exists(unzip_mod_dir):
+            raise Exception(f'The unzip directory ({unzip_mod_dir}) already exists! Please change it.')
+
+    # Unzip steam archive
+    for file in os.listdir(args.steam):
+        if file.endswith('.zip'):
+            unzip(os.path.join(args.steam, file), unzip_mod_dir)
 
     # Remove old English files
     i = 0
@@ -42,6 +62,7 @@ def main(args):
                 shutil.copyfile(os.path.join(unzip_mod_dir, 'localisation', 'replace', file),
                                 os.path.join(args.mod, 'localisation', 'replace', file))
     print('{0} new files copied'.format(i))
+
 
 if __name__ == "__main__":
     # execute only if run as a script
